f1dbee19dbfd906592a29924cc788eb9ff6f2920
saddu771 <62069614+saddu771@users.noreply.github.com>
2021-05-12 11:26:18 +0530

    Create hostname1.sh

---
+++ hostname1.sh
@@ -0,0 +1,116 @@
+@@x.@8>@@@@@@��88@8@@@PP ``h	 ``��TT@T@DDP�td��@�@��Q�tdR�td``/lib64/ld-linux-x86-64.so.2GNUGNU$0�`wX�+ �↖�7��!0�@!#ĉ��ݣkĹ�@9���j" 4:\c��{U���> �A/������
+                                                                 MM F�*�! &�! &� &�@&libc.so.6sprintfperror__isoc99_sscanfforktime_exitgetpidkillstrdupgetppidcallocstrlenmemset__errno_locationmemcmpputenvmemcpymallocgetenvstderrexecvpptracewaitpidcloseopenfprintfatollstrerror__libc_start_main__environ__xstatGLIBC_2.7GLIBC_2.14GLIBC_2.2.5_ITM_deregisterTMCloneTable���art___ITM_registerTMCloneTableii
+         ui	�`�`	�`�` &`#@&`$ `  `( `0 `8 `@ `H P `
+X `
+   ` `
+p `x `� `� `� `� `� `� `� `� `� `� `� `� `� `� `� ` ��H�H�q H��t��H���5r �%t @�%r h������%j h������%b h������%Z h������%R h������%J h������%B h������%: h�p����%2 �`����%* h	�P����%" h
+�@����% h
+           �0����% h
+                      � ����%
+�����% h�����%� h������%� h������%� h������%� h������%� h������%� h������%� h������%� h������%� h�p����%� h�`����%� h�P����%� h�@����%� h�0�����1�I��^H��H���PTI��@@H���@H���@�Z �����f.��H�=y H�r H9�tH�& H��t	�����H�=I H�5B H)�H��H��H��?H�H��tH�� H����fD�����=- uUH���z���� ]Ð�Df.�����UH��� �
+ � �� �� �� ���� H���`&`�� ���� �� ��u̐]�UH��H�}��u�H�E�H�E����� ��H���`&`�E��� �E�Ј �v ����}���Hc�H�E�H���[ ЈS �L ���@ ��H���`&`Hc���`&`�& ��H��U���`&`� ��� �� ���P���H�E��m��}��7����]�UH��H�}��u�H�E�H�E����� ���� �� ��H���`&`�E��� �E�Ј� �� ���x ��H���`&`Hc���`&`�] ��H��U���`&`�F ��H���`&`E�H�E���E�H���`&`1�H�E��H�E��m��}��F����]�UH��H��0H������H��p���H������H��H�����y
+�������H���������H���C���H��x���H������H��p���H������H�E�H�����E��������E������H�E�H�����H�E�H��8���H�E�H��H���H��������H����������UH��H�}�H�u��H�EH�}�t/H�E�H�H��t#H�E�H�H9E�u��H�E�H�H�E�H�H�EH�}�t
+           H�E�H�H��uِ]�UH��H��@������H�E��@�1���H�H�U�H��H��H1E�H�U�H��������@H�Ǹ�C���H������H�������H�E�H������H��������E�H�}�uOH�������E�H�H�<������H�E���H�¾�@������H������H�������H����������H������H������H������H�E�I����@H�Ǹ�:����E��}�uFH������H9E�u9�E���H�H�P�H�E�H�H�{ H��H���r���������������)Ѓ���������UH��H��pH�}��s����E��}�����}����'����E��U�H�E���@H�Ǹ�����������H�E���H�Ǹ������������E��}�u6�e������t*�E����ƿ��P���H�������E��}�t�E�����������E�H���Q����E��	��������E�������E���������9E�t��H�E�H���������������UH��SH��H�}�H�u�H�E�H�H�E��9������!`������*��"`�[�����
+                            #`�L����� ��t&�
+                                           #`�����H�ÿ�$���H9�}
+��"`���
+�2!`�
+     ������"`�������"!`�������
+                                #`��������"`��������"`�������#`�������#`��"`�(�����t
+��"`���E���������EԾ�.#`�t����}�y
+�.#`���E���
+H�H�������H�E�H�}�u
+ ��u�2!`�������t�����
+#`��������I#`�������?!`�������?!`�������&`�������&`�?!`�)�����t
+�?!`��������H�E�H�}�u
+���H�E��� H�������H�E�H���I#`H�������N��
+ ��t;��C���H�E�H�}�u
+���H�U�H�E��"!`H�Ǹ�����H�E�H�E��E��E��P�U�H�H��H�E�H�H�E�H�H��}�t,�?
+#`��                                                                      ��t!�E��P�U�H�H��H�E�H�H�
+     ��t!�E��P�U�H�H��H�E�H�H��"`�E��P�U�H�H��H�E�H�H�E�H���
+                                                                    ��t!�E��P�U�H�H��H�E�H�H�
+                                                                                                #`�}�~�E����E��;�E��P�U�H�H��H�E�H�
+                                                                                                                                      �E��P�U�H�H��H�E�H�H�H��E�;E�|��E�H�H��H�E�H�H�H�E�H�ƿ2!`�����2!`H��H[]�UH��ATSH���}�H�u�H�E�H�H�������H�E�H�H�U��E�H�։������H�H�E�H�H�H��t
+ I��M�ྵ@H�Ǹ������H��[A\]�f���AWI��AVI��AUA��ATL�% UH�- SL)�H��o���H��t1��L��L��D��A��H��H9�u�H�[]A\A]A^A_Ðf.����f.����H��H����,�����H�H��x%lx=%lu %d%lu %d%c/proc/%d/as<null>: %s%s%s: %s
+;�������������������������
+                             ����,����L^���l���������)����8����
+                                                                      ���|���\����tzRx
+                                                                                               �����/D0���$D����FJ
+W                                                                                                                        �?;*3$"l����\A�C
+�������A�C
+�������A�C
+��C����A�C
+Z�����_A�C
+  
+F)���KA�C
+,T���!A�C
+E�U���A�C
+G���@����A�C
+       D�����eF�E�E �E(�H0�H8�G@n8A0A(B BB��������@@@
+                                                                        p
+d@``���o�@H@�@                                                   @
+g
+   `�@�	���@���o���o�@`�
+                                    @�
+                                      @�
+                                        @�
+                                          @�
+                                            @�
+                                              @
+                                                @
+                                                  @&
+                                                    @6
+                                                      @F
+                                                        @V
+                                                          @f
+                                                            @v
+                                                              @�
+                                                                @�
+                                                                  @�
+                                                                    @�
+                                                                      @�
+                                                                        @�
+                                                                          @�
+                                                                            @�
+RI�#���84�:&��ڕ>���Zl�� ��c ���Q��-       ���_���z�jkN .o���Ѥm�u��{��@4�bBa� $���
+                                                                                          g-x� A����n
+�7�&'��v���V��LQ'�HX����A'�"<                                                                        �$�Yǎ�)���@��!�-N�n^~/|!Ԡ�.�o�p������,G��u<�?�N��\�����z��0
+                              t�2�[ݙ�3"�t�i�`�
+                                                 ��4�Wc���
+����53���I�����I���ǫ���Cλ �T��S/>����7��d�+[k�;$�Y��	
+�KD6�¦J/Cy$Y{�s��?��A�B΅h�vH�:�TֹeL�B�@A�}�`�`�"땠V�c
+�5J�F?T�
+ru��8�6��`�Iq�w�f���v�T���G
+"�`���h�b��F_��y�`�n6�Kܵ�η��I�Aq���ڧSk����ޖ��6���
+��S��FM�                                           " 	\�	��p��j�:i��W5񞧨R�����x��٧~ka��?��r���?���"�Z��O�D�I P1��bi#K��
+         >?F�"���R�M-N��Pd����jj)�^�ʫ���@w�r�O����:�-�}V+����
+                                                                      �8�	dZ�l�s{�������b���e%c.QT^�e)���c9�@�Si�	l8;dI;��ǎy7�Z�E������v���#X�-󪅦��7H������)��9O���_r]��]���ٮu��vs��V���J��5HWt{V6ơ#���)�a���[8�:\y�.�h���UB�����G�i��:$�*�a�될{�L|I�\��4v@���ma�bl���i�L��&�^ ���1�4C9l�9��Uj߃�զ��v��vd6�5G���7"xf��
+��FM�x�V?��-�}*2�X�	> ��bI]{݆X����GCC: (GNU) 8.3.1 20190507 (Red Hat 8.3.1-4)
+@�GA$3a1`                                                                     GA$3h876`
+@GA$gcc 8.3.1 20190507
+                         GA*GOW*GA*GA+stack_clashGA*cf_protectioGA*FORTIFY�GA+GLIBCXX_ASSERTIONGA*GAGA+omit_frame_pointer
+                                                                                                                                                 GA*
+                                                                                                                                                         GA!stack_realign
+                                                                                                                                                                            GA*GOW*EGA+stack_clashGA+GLIBCXX_ASSERTIONSGA!stack_realign
+                                                     GA*GOW*EGA+stack_clashGA+GLIBCXX_ASSERTIONSGA!stack_realign
+                                                                                                                           GA*GOW*EGA+stack_clashGA+GLIBCXX_ASSERTIONSGA!stack_realign
+    GA*GOW*EGA+stack_clashGA+GLIBCXX_ASSERTIONSGA!stack_realigGA$3a1p
+                                                                                    @�
+                                                                                      GA$3a1d@l@
+                                                                                                      GA$3p876�@E@GA$gcc 8.3.1 20190507
+                                                                                                                                                GA*GOW*GA*GA+stack_clashGA*cf_protectioGA*FORTIFYGA+GLIBCXX_ASSERTIONGA*GAGA+omit_frame_pointer
+                                                                            GA*
+                                                                                    GA!stack_realign
+                                                                                                       GA$3p876P@d@GA$gcc 8.3.1 20190507
+                                                                                                                                                GA*GOW*EGA*GA+stack_clashGA*cf_proGA*FORTIFYGA+GLIBCXX_ASSERTIONSGA*GAGA+omit_frame_pointer
+                                                                    GA*
+                                                                           GA!stack_realigGA$3a1�
+                                                                                                        @�
+                                                                                                          GA$3a1l@q@.shstrtab.interp.note.ABI-tag.note.gnu.build-id.gnu.hash.dynsym.dynstr.gnu.version.gnu.version_r.rela.dyn.rela.plt.init.text.fini.rodata.eh_frame_hdr.eh_frame.init_array.fini_array.dynamic.got.got.plt.data.bss.comment.gnu.build.attributes
+                                                                                                                                                                          8@8T@T !t@t$4���o�@�>
+                    �@�xFH@HgN���o�@�J[���o@j@tB@�~p
+                                                                            @p
+                                                                              y�
+                                                                                    @�
+`                                                                                    ��`
+�x@x��@���H@H��`���`�� `�!`!  &`&H �0&,�h'�H&$l-	
